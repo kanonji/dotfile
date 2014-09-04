c671c194b3f991d8d4aca67c334851f2d3d921df
kanonji <kanonji@users.noreply.github.com>
2014-09-04 17:38:46 +0900

    relaunch, hint, push

--- .slate.js
+++ .slate.js
@@ -1,2 +1,30 @@
 S.log('[SLATE] ----------- Start Loading Config -----------');
+
+// Config
+S.configAll({
+  defaultToCurrentScreen: true,
+
+  windowHintsIgnoreHiddenWindows: false,
+  windowHintsShowIcons: true,
+});
+
+// Key binding
+S.bindAll({
+  'r:ctrl,cmd': S.op('relaunch'),
+  ';:ctrl,cmd': S.op('hint'),
+
+  'h:ctrl,cmd': S.op('push', {direction: 'left', style: 'bar-resize:screenSizeX/2'}),
+  'k:ctrl,cmd': S.op('push', {direction: 'up', style: 'bar-resize:screenSizeY/2'}),
+  'j:ctrl,cmd': S.op('push', {direction: 'down', style: 'bar-resize:screenSizeY/2'}),
+  'l:ctrl,cmd': S.op('push', {direction: 'right', style: 'bar-resize:screenSizeX/2'}),
+  'm:ctrl,cmd': S.op('push', {direction: 'right', style: 'bar-resize:screenSizeX/3*2'}),
+  'n:ctrl,cmd': S.op('push', {direction: 'left', style: 'bar-resize:screenSizeX/3'}),
+
+  'g:ctrl,cmd': S.op('grid'),
+});
+
+// Test Cases
+// S.src(".slate.test", true);
+// S.src(".slate.test.js", true);
+
 S.log('[SLATE] ----------- Finished Loading Config -----------');
